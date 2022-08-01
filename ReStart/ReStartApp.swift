4aa63f37b9f2c46c3b9743f0996f47088f592a92
KhuePM <minhkhuepham99@gmail.com>
2022-08-01 23:00:29 +0700

    Initial Commit

---
+++ ReStart/ReStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReStartApp.swift
+//  ReStart
+//
+//  Created by Phạm Minh Khuê on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
